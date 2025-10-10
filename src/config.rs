5038d9fce59d68b0ca2e644edd97a37e04324192
caiya <admin@zouxj.cn>
2025-10-10 08:16:31 +0800

    修改内置api和key

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["yc.yaohst.com"];
+pub const RS_PUB_KEY: &str = "8vlbnmsns13VPgclki7olXhgQAH5NMdhJXht4v+atbE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
